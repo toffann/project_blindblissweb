c71b70c795a718aa81a069efb330345ddf94c2a7
maxleen2 <caramel.lattee20@gmail.com>
2025-06-23 11:43:58 +0700

    menambahkan data transaksi pembelian

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -28,7 +28,7 @@ if (uri_string() != "") {
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
- 
+
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
 
@@ -70,6 +70,13 @@ if (uri_string() != "") {
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
+  <!-- jQuery -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+  <!-- Select2 -->
+  <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+  <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
   <!-- Vendor JS Files -->
   <script src="<?= base_url() ?>NiceAdmin/assets/vendor/apexcharts/apexcharts.min.js"></script>
   <script src="<?= base_url() ?>NiceAdmin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -85,6 +92,7 @@ if (uri_string() != "") {
   <!-- Template Main JS File -->
   <script src="/NiceAdmin/assets/js/main.js"></script>
 
+  <?= $this->renderSection('script') ?>
 
 </body>
 
